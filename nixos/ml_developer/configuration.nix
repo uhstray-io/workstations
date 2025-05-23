38e3635c747967d2523c7b4e5466aeb6d8858ae7
Joseph A. Wisneski IV <stray@uhstray.io>
2025-05-23 15:22:02 -0400

    removed unstable tag from brave

--- nixos/ml_developer/configuration.nix
+++ nixos/ml_developer/configuration.nix
@@ -138,7 +138,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   
   # Brave Web Browser
-  unstable.brave
+  brave
 
   # cURL 
   curl
